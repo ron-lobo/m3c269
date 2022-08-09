031ead39ee095eeaba13d5bba541f6bd668a9c4a
ron <ron.lobo@yahoo.com>
2022-08-09 09:49:45 +0100

    0007: printf format examples

--- src/main/java/org/ron/m3/intro/Mutability.java
+++ src/main/java/org/ron/m3/intro/Mutability.java
@@ -2,7 +2,7 @@ package org.ron.m3.intro;
 
 public class Mutability {
 
-    public static final double PI = 3.14;
+    public static final double PI = 22 / 7.0;
 
     public static void main(String[] args) {
         m1();
@@ -16,34 +16,29 @@ public class Mutability {
         String str = "Hi";
         StringBuilder sb = new StringBuilder("Hello");
 
-        showVariables("Before calling m2()", i, i1, str, sb);
-        m2(i, i1, str, sb);
-        showVariables("After calling m2()", i, i1, str, sb);
+        showVariables("Before calling m2()", i, i1, str, sb, PI);
+        m2(i, i1, str, sb, PI);
+        showVariables("After calling m2()", i, i1, str, sb, PI);
 
         str += " cruel";
         str = str + " world";
         i++;
         i1++;
         i1 += 5;
-        showVariables("After reassignments", i, i1, str, sb);
+        double d = PI * -PI;
+        showVariables("After reassignments", i, i1, str, sb, d);
     }
 
-    private static void showVariables(String text, int i, Integer i1, String str, StringBuilder sb) {
-//        System.out.println();
-//        System.out.println(text);
-//        System.out.println("i = " + i);
-//        System.out.println("i1 = " + i1);
-//        System.out.println("sb = " + sb);
-//        System.out.println("str = " + str);
-
-        System.out.printf("%5$s: i=%1$d, i1=%2$d, sb=%4$s, str=%3$s", i, i1, str, sb, text);
+    private static void showVariables(String text, int i, Integer i1, String str, StringBuilder sb, double d) {
+        System.out.printf("%5$s: i=%1$d, i1=%2$d, sb=%4$s, str=%3$s, d=%6$+07.2f %n", i, i1, str, sb, text, d);
     }
 
-    public static void m2(int i, Integer j, String str, StringBuilder sb) {
+    public static void m2(int i, Integer j, String str, StringBuilder sb, double d) {
         i = 99;
         j = 999;
         sb.append(" World");
-//        sb = new StringBuilder("hi World");
+        sb = new StringBuilder("hi World");
         str += " there";
+        d *= d;
     }
 }
