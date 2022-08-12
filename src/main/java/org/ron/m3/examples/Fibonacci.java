ba960176376b52bc3067393bc3377ec65fb5fb68
ron <ron.lobo@yahoo.com>
2022-08-12 11:01:36 +0100

    0015: fixed Fibonacci.java

--- src/main/java/org/ron/m3/examples/Fibonacci.java
+++ src/main/java/org/ron/m3/examples/Fibonacci.java
@@ -6,14 +6,30 @@ public class Fibonacci {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter a number: ");
-        int pos = scanner.nextInt();
-        int result = getItemInSeries(pos);
-        System.out.println("result = " + result);
+        while (true) {
+            System.out.println("Please enter a number: ");
+            int pos = scanner.nextInt();
+            long result = getItemInSeries(pos);
+            System.out.println("result = " + result);
+        }
     }
 
-    // [0,] 1, 1, 2, 3, 5, 8, 13, 21, ...
-    public static int getItemInSeries(int pos) {
-        return -1;
+    public static long getItemInSeries(int pos) {
+        if (pos <= 2) {
+            return 1;
+        }
+        long x = 1, y = 1;
+        for (int i = 0; i < pos - 3; i++) {
+            long z = x + y;
+            x = y;
+            y = z;
+        }
+        return x + y;
     }
+
+//    // [0,] 1, 1, 2, 3, 5, 8, 13, 21, ...
+//    public static int getItemInSeries_01(int pos) {
+//        int[] results = new int[]{1, 1, 2, 3, 5};
+//        return results[pos];
+//    }
 }
